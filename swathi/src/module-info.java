6db50d685c9227ec4cd9ff58fa68a3e5e9800cb5
swathikuppala <kswathi515@gmail.com>
2022-03-10 10:03:59 +0530

    print 0 to 10 numbers

---
+++ swathi/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Swathi
+ *
+ */
+module swathi {
+}
---
+++ swathi/src/swathi/jenkins2.java
@@ -0,0 +1,13 @@
+package swathi;
+
+public class jenkins2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0; i<10; i++) {
+			System.out.println(i);
+		}
+
+	}
+
+}
