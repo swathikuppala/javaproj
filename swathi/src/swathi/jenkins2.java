491de1382ed625cfbc6d653b95f848960b29a38d
swathikuppala <kswathi515@gmail.com>
2022-03-17 11:00:47 +0530

    committed

--- swathi/src/swathi/jenkins2.java
+++ swathi/src/swathi/jenkins2.java
@@ -5,7 +5,7 @@ public class jenkins2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i=1; i<108; i++) {
-			System.out.println("sairam");
+			System.out.println("om sai ram");
 		}
 
 	}
