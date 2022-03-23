3471e50c8e2d70d22cbf0297b3d34c9a95e6757a
swathi kuppala <38715985+swathikuppala@users.noreply.github.com>
2022-03-23 23:01:37 +0530

    Update jenkinsdemo.java

--- swathi/src/swathi/jenkinsdemo.java
+++ swathi/src/swathi/jenkinsdemo.java
@@ -5,7 +5,7 @@ public class jenkinsdemo{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      for (int i=1;i<10;i++) {
-	System.out.println("Welcome to jenkins");
+	System.out.println("Welcome to jenkins integration");
 }
 	}
 
