457fbf6b5ac63c7772915fa65f41822b5fbec2d0
swathi kuppala <38715985+swathikuppala@users.noreply.github.com>
2022-03-23 22:56:53 +0530

    Update jenkins2.java

--- swathi/src/swathi/jenkins2.java
+++ swathi/src/swathi/jenkins2.java
@@ -4,7 +4,7 @@ public class jenkins2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=1; i<10; i++) {
+		for(int i=0; i<10; i++) {
 			System.out.println("git webhook integration with jenkins");
 		}
 
