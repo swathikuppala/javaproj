599ee1080eb5d3acc00880a781dc701db47ac5f2
swathikuppala <kswathi515@gmail.com>
2022-03-23 22:38:53 +0530

    added

--- swathi/src/swathi/jenkins2.java
+++ swathi/src/swathi/jenkins2.java
@@ -4,7 +4,7 @@ public class jenkins2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=1; i<108; i++) {
+		for(int i=1; i<100; i++) {
 			System.out.println("om sai ram");
 		}
 
